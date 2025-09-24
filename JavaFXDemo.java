4880550c044c3cd72b04a69e02005e69e2fa9514
theta148 <mihusunny@gmail.com>
2025-09-23 23:03:21 -0400

    Add files via upload
    Implemented the calculations for costs.

--- JavaFXDemo.java
+++ JavaFXDemo.java
@@ -18,7 +18,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author 6316704
+ * @author Mihir
  */
 public class JavaFXDemo extends Application{
 
@@ -40,43 +40,57 @@ public class JavaFXDemo extends Application{
         
         Label daysLabel = new Label("Number of days on the trip: ");
         TextField daysField = new TextField();
+        String dayStr = daysField.getText();
         gp.add(daysLabel, 0, 0);
         gp.add(daysField, 1, 0);
         
         Label airfaresLabel = new Label("Amount of airfares, if any: ");
         TextField airfaresField = new TextField();
+        String airfareStr = airfaresField.getText();
         gp.add(airfaresLabel, 0, 1);
         gp.add(airfaresField, 1, 1);
         
         Label carRentalFeesLabel = new Label("Amount of car rental fees, if any: ");
         TextField carRentalFeesField = new TextField();
+        String carRentalFeeStr = carRentalFeesField.getText();
         gp.add(carRentalFeesLabel, 0, 2);
         gp.add(carRentalFeesField, 1, 2);
         
         Label milesDrivenLabel = new Label("Number of miles driven, if a private vehicule was used: ");
         TextField milesDrivenField = new TextField();
+        String mileStr = milesDrivenField.getText();
         gp.add(milesDrivenLabel, 0, 3);
         gp.add(milesDrivenField, 1, 3);
         
         Label parkingFeesLabel = new Label("Amount of parking fees, if any: ");
         TextField parkingFeesField = new TextField();
+        String parkingFeeStr = parkingFeesField.getText();
         gp.add(parkingFeesLabel, 0, 4);
         gp.add(parkingFeesField, 1, 4);
         
         Label taxiChargesLabel = new Label("Amount of taxi charges, if any: ");
         TextField taxiChargesField = new TextField();
+        String taxiChargeStr = taxiChargesField.getText();
         gp.add(taxiChargesLabel, 0, 5);
         gp.add(taxiChargesField, 1, 5);
         
         Label registrationFeesLabel = new Label("Conference or seminar registration fees, if any: ");
         TextField registrationFeesField = new TextField();
+        String registrationFeeStr = registrationFeesField.getText();
         gp.add(registrationFeesLabel, 0, 6);
         gp.add(registrationFeesField, 1, 6);
         
         Label lodgingChargesLabel = new Label("Lodging charges, per night: ");
         TextField lodgingChargesField = new TextField();
+        String lodgingStr = lodgingChargesField.getText();
         gp.add(lodgingChargesLabel, 0, 7);
         gp.add(lodgingChargesField, 1, 7);
+        
+        //double mealCosts = 37;
+        //double parkingFees = 10 * Double.parseDouble(dayStr);
+        //double taxiCharge = 20 * Double.parseDouble(taxiChargeStr);
+        //double lodgingCharge = 95 * Double.parseDouble(lodgingStr);
+        //double vehiculeCharge = 0.27 * Double.parseDouble(mileStr);
         
         Scene scene = new Scene(gp, 500, 400);
         scene.getStylesheets().add("demo1.css");
